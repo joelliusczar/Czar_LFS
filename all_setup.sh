a34565d660f6087847d2a8bba89c925f45c3500a
Joel Gillete <joelliuscaesar@gmail.com>
2019-01-20 09:37:11 +0000

    Created another log file that logs everything. Added logic to prevent unwanted file cleanup. But also added some more things to clean up. Fixed a bug where we switched dir but never switched back.

--- all_setup.sh
+++ all_setup.sh
@@ -1,34 +1,49 @@
 #!/bin/bash
 
-if [ "$EUID" -ne 0 ]; then
+if [ "$EUID" -ne 0 ]; then #assuming that root uid is 0
   echo "Plase Run as Root"
   exit 1
 fi
 
 while [ "$#" -gt 0 ]; do
-case "$1" in
---skip_setup) skip_setup=1 ;;
---volgroup=*)
-vgarg="${1#*=}"
-;;
---checkpoint=*)
-ch_arg="${1#*=}"
-;;
---at_test=*)
-testarg="${1#*=}"
-;;
-*) : ;;
-esac
-shift
+  case "$1" in
+    --skip_setup) skip_setup=1 ;;
+    --volgroup=*)
+      vgarg="${1#*=}"
+      ;;
+    --checkpoint=*)
+      ch_arg="${1#*=}"
+      ;;
+    --at_test=*) 
+      #this should be the file name of a script that you want to skip to. check ch5_scripts.sh ch6_scripts.sh
+      #for reference
+      testarg="${1#*=}"
+      ;;
+    --clean_out_uid=*) 
+      #provide the user id of someone else who might own files in the sources dir that need to be cleaned out
+      #from a previous install attempt. It's usually root
+      clean_out_uid="${1#*=}"
+      ;;
+    *) : ;;
+  esac
+  shift
 done 
 volgroup="${vgarg:-vglfs}"
 checkpoint="${ch_arg:-0}"
 at_test="${testarg}"
 export LFS=/mnt/lfs LFS_SH=/lfs_scripts
 log_path=$LFS/lfs_install.log
+full_log_path=$LFS/lfs_full.log
 ch5_chk=0
 ch6_pre_chk=1
 ch6_chk=2
+
+create_logs() {
+  echo "" > "$log_path"
+  chmod u=rw,g=rw,o=rw  "$log_path"
+  echo "" > "$full_log_path"
+  chmod u=rw,g=rw,o=rw  "$full_log_path"
+}
 
 ch5_install() {
   #checkpoint should be less than to enter branch
@@ -39,17 +54,30 @@ ch5_install() {
     if [ "$skip_setup" -lt 1 ] && [ -z "$at_test" ]; then
       bash setup1.sh --volgroup="$volgroup" ||
       { echo "Setup 1 crashed!"; exit 1; }
-    fi
-    bash clean_up_lfs_dir.sh &&
-    echo "" > "$log_path" && #clean up will have deleted this file, so we create it following clean up
-    chmod u=rw,g=rw,o=rw  "$log_path" &&
+    fi 
+    if [ -z "$at_test" ]; then # if we have a specific script, we don't this guy deleting all the previous stuff
+      #the clean up script is mostly for cleaning up stuff that happened in ch6, if we made it there in a previous
+      #install attemp.
+      bash clean_up_lfs_dir.sh
+    fi &&
+    if [ -n "$clean_out_uid" ]; then 
+      #this bit of logic needs to be separated from the rest of the clean up code because 
+      #we only want the clean up code to execute when we don't have a specific test.
+      #but sometimes we want this to execute even if there is a specific test to start at
+      
+      find "$LFS"/sources -maxdepth 0 -type d -user "$clean_out_uid" -exec rm -rf {} \;
+      
+    fi &&
+    create_logs && #clean up will have deleted these logs, so we create it following clean up
     cp -rv ch5_scripts install_help.sh /home/lfs/ &&
+
     sudo -u lfs env -i auto_lfs=t log_path="$log_path" TERM="$TERM" PS1='\u:\w\$ ' \
-    bash -l script_runner.sh --src_script="ch5_scripts.sh" --script_dir="ch5_scripts" --at_test="$at_test"  
+      bash -l script_runner.sh --src_script="ch5_scripts.sh" --script_dir="ch5_scripts" --at_test="$at_test"  
+
   else
     echo "skipping ch 5" | tee -a "$log_path"
-    #let it return 0 because skipping is not necessarily a bad thing. Otherwise, it will error out when we
-    #try to skip to further branches
+    #let it return 0 because skipping is not necessarily a bad thing. Otherwise, it will error out
+    #when we try to skip to further branches
   fi
 }
 
@@ -60,8 +88,11 @@ ch6_pre() {
     bash change_owner.sh &&
     mkdir -pv "$LFS""$LFS_SH" &&
     if [ -z "$at_test" ]; then
-      cd ch6_scripts && #change to dir to run 00_prepare_virtual_kernel_fs.sh
-      LFS="$LFS" bash 00_prepare_virtual_kernel_fs.sh
+      pushd ch6_scripts && #change to dir to run 00_prepare_virtual_kernel_fs.sh
+        LFS="$LFS" bash 00_prepare_virtual_kernel_fs.sh
+        xs="$?"
+      popd
+      return "$xs"
     fi 
   else
     echo "skipping pre ch6" | tee -a "$log_path"
@@ -79,11 +110,12 @@ ch6_chroot_install() {
   fi
 }
 
-echo "" > "$log_path" &&
+create_logs &&
+exec 1> >(tee "$full_log_path") 2>&1 &&
 # if blocks will continue even if the condition is false, but we want the logic to stop code
 # so using && with a block enables us to do that
 ch5_install && {
-  echo 'ch5 finished' | tee -a "$log_path"
+  echo 'ch5 finished' | tee -a "$log_path" 
   #if code goes in here then it implies that at_test was not meant for further sections
   #so erase at_test if this is our chapter and we didn't just skip it, since skipping will also cause it
   #to go down this branch
--- install_help.sh
+++ install_help.sh
@@ -3,7 +3,7 @@
 #As long as we source this file, $0 should be the script calling this
 #rather than 'install_help.sh' itself
 self_file="$0"
-log_path=${log_path:-"$LFS/lfs_install.log"}
+log_path=${log_path:-"${LFS:-}/lfs_install.log"} #when chrooting, LFS is an empty string and I just want that explicit
 
 #these are left empty for scripts sourcing this one to overwrite
 extra_pre_success() { :; }
@@ -14,6 +14,9 @@ extra_pre_run() { :; }
 extra_post_run() { :; }
 extra_pre_term() { :; }
 extra_post_term() { :; }
+
+#There's a lot of usage of tee scattered around. I could solve that with an exec 1> >(tee "$log_path")
+#but I only want outputs that I generate to go to "$log_path" whereas the above will capture all of it
 
 success_cleanup() {
   echo "Winner is $app" | tee -a "$log_path"
@@ -47,20 +50,27 @@ install_app_nest() {
   echo "running $app" | tee -a "$log_path"
   cat <(extra_pre_run) | tee -a "$log_path"
 	cd "$sources_dir"
+  if [ -e "$app" ] && [ $(stat -c %u "$app") != $(id -u lfs 2>/dev/null || echo '_') ]; then
+    echo "$app already exists and it belongs to someone else"
+    return 1  
+  fi
 	rm -rf "$app"
 	tar -xf "$tar_name".tar.xz 2>/dev/null || tar -xf "$tar_name".tar.gz  2>/dev/null ||
-    tar -xf "$tar_name".tar.bz2
+    tar -xf "$tar_name".tar.bz2 
 	cd "$app" &&
+
   #I apologize for the redirection mess below
   #I had 4 goals 
   # 1: I wanted to be able to log the time it took to run a script
   # 2: I also wanted to still print the time to the stdout
   # 3: I did not want to log the output from the script itself
   # 4: I wanted to retain the exit status of the script
+
 	((((time {	
 		(install_app 1>&3 2>&4; echo $? 1>&5;); 
 	}) 2>&1) | tee -a "$log_path" 1>&4) 5>&1 | (read xs; exit "$xs";)) 3>&1 4>&2
   xs=$? #store exit status so that extra_post_run below doesn't fuck it up
+
   cat <(extra_post_run) | tee -a "$log_path"
   echo "Exit status is $xs" | tee -a "$log_path"
   return "$xs";
